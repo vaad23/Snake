e0e4a53ed4eb1011fc7adcdba181a18a4266e57d
vaad23 <vaad23@mail.ru>
2021-02-08 15:11:15 +0300

    add Snake.exe

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -10,17 +10,20 @@ public class Game : MonoBehaviour
     [SerializeField] private Snake _snake;
     [SerializeField] private GameObject _gameOver;
     [SerializeField] private Button _restart;
+    [SerializeField] private Button _exit;
 
     private void OnEnable()
     {
         _snake.Died += GameOver;
         _restart.onClick.AddListener(Restart);
+        _exit.onClick.AddListener(Exit);
     }
 
     private void OnDisable()
     {
         _snake.Died -= GameOver;
         _restart.onClick.RemoveListener(Restart);
+        _exit.onClick.RemoveListener(Exit);
     }
 
     private void Start()
@@ -37,5 +40,10 @@ public class Game : MonoBehaviour
     private void Restart()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void Exit()
+    {
+        Application.Quit();
     }
 }
